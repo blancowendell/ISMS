374e6a6fe668aad2ec4ddd3a2b50b394fa2dcde8
blancowendell <blanco.wendell15@gmail.com>
2024-08-09 16:43:31 +0800

    CREATE USER FUNCTIONS

--- routes/createuser.js
+++ routes/createuser.js
@@ -4,6 +4,7 @@ var express = require("express");
 const { MessageStatus, JsonErrorResponse, JsonSuccess, JsonWarningResponse } = require("./repository/response");
 const { SelectStatement, InsertStatement, GetCurrentDatetime } = require("./repository/customhelper");
 const { InsertTable, Select } = require("./repository/dbconnect");
+const { Encrypter } = require("./repository/crytography");
 //const { Validator } = require("./controller/middleware");
 var router = express.Router();
 
@@ -13,3 +14,76 @@ router.get("/", function (req, res, next) {
 });
 
 module.exports = router;
+
+
+
+router.post("/save", async (req, res) => {
+  try {
+    const { studentid } = req.body;
+    let username = req.body.username;
+    let password = req.body.password;
+    let status = "Active";
+    let accesstype = "2";
+    const createdate = GetCurrentDatetime();
+
+    Encrypter(password, async (err, encrypted) => {
+      if (err) {
+        console.error("Error: ", err);
+        res.json({ msg: "error" });
+      } else {
+        let sql = InsertStatement("master_user", "mu", [
+          "studentid",
+          "username",
+          "password",
+          "accesstype",
+          "createdate",
+          "status",
+        ]);
+
+        let data = [[studentid, username, encrypted, accesstype, createdate, status]];
+        let checkStatement = SelectStatement(
+          "select * from master_user where mu_studentid=? and mu_accesstype=?",
+          [studentid, accesstype]
+        );
+        Check(checkStatement)
+        .then((result) => {
+          console.log(result);
+          if (result != 0) {
+            return res.json(JsonWarningResponse(MessageStatus.EXIST));
+          } else {
+            InsertTable(sql, data, (err, result) => {
+              if (err) {
+                console.log(err);
+                res.json(JsonErrorResponse(err));
+              }
+
+              res.json(JsonSuccess());
+            });
+          }
+        })
+      .catch((error) => {
+        console.log(error);
+        res.json(JsonErrorResponse(error));
+      });
+      }
+    });
+  } catch (error) {
+    console.error("Error: ", error);
+    res.json({ msg: "error" });
+  }
+});
+
+
+
+
+//#region FUNCTION
+function Check(sql) {
+  return new Promise((resolve, reject) => {
+    Select(sql, (err, result) => {
+      if (err) reject(err);
+
+      resolve(result);
+    });
+  });
+}
+//#endregion
--- routes/login.js
+++ routes/login.js
@@ -11,3 +11,148 @@ router.get('/', function(req, res, next) {
   });
   
 module.exports = router;
+
+
+router.post("/login", (req, res) => {
+  try {
+    const { username, password, accesstypeid } = req.body;
+
+    Encrypter(password, (err, encrypted) => {
+      if (err) console.error("Error: ", err);
+
+      let sql = `SELECT 
+          mu_employeeid AS employeeid,
+          CONCAT(me_firstname, ' ', me_lastname) AS fullname,
+          ma_accessname AS accesstype,
+          mu_status AS status,
+          me_profile_pic AS image,
+          me_jobstatus AS jobstatus,
+          md_departmentid AS departmentid,
+          mu_isgeofence AS isgeofence,
+          md_departmentname AS departmentname,
+          mp_positionname AS position,
+          ma_accessid AS accesstypeid,
+          mgs_id AS geofenceid,
+          (SELECT 
+          GROUP_CONCAT(us_subgroupid) AS subgroupids
+        FROM 
+          user_subgroup
+          WHERE 
+          us_userid = (SELECT mu_userid 
+                      FROM master_user 
+                      WHERE mu_username = '${username}'
+                      AND mu_password = '${encrypted}'
+                      AND mu_accesstype = '${accesstypeid}')) as subgroupid
+      FROM 
+          master_user
+      INNER JOIN 
+          master_access ON mu_accesstype = ma_accessid
+      LEFT JOIN 
+          master_employee ON mu_employeeid = me_id
+      LEFT JOIN 
+          master_department ON md_departmentid = me_department
+      LEFT JOIN 
+          master_position ON mp_positionid = me_position
+      LEFT JOIN 
+          user_subgroup us ON mu_userid = us.us_userid
+      LEFT JOIN master_geofence_settings ON mgs_departmentid = me_department 
+      WHERE 
+          mu_username = '${username}' 
+          AND mu_password = '${encrypted}'
+          AND mu_accesstype = '${accesstypeid}'
+      GROUP BY 
+          mu_employeeid,
+          me_firstname,
+          me_lastname,
+          ma_accessname,
+          mu_status,
+          me_profile_pic,
+          me_jobstatus,
+          md_departmentid,
+          mu_isgeofence,
+          md_departmentname,
+          mp_positionname,
+          ma_accessid,
+          mgs_id
+      LIMIT 1`;
+
+      mysql.mysqlQueryPromise(sql)
+        .then((result) => {
+          if (result.length !== 0) {
+            const user = result[0];
+
+            if (
+              user.jobstatus === "probitionary" ||
+              user.jobstatus === "regular" ||
+              user.jobstatus === "apprentice"
+            ) {
+              if (user.status === "Active") {
+                let data = UserLogin(result);
+
+                data.forEach((user) => {
+                  req.session.employeeid = user.employeeid;
+                  req.session.fullname = user.fullname;
+                  req.session.accesstype = user.accesstype;
+                  req.session.image = user.image;
+                  req.session.departmentid = user.departmentid;
+                  req.session.isgeofence = user.isgeofence;
+                  req.session.departmentname = user.departmentname;
+                  req.session.position = user.position;
+                  req.session.jobstatus = user.jobstatus;
+                  req.session.geofenceid = user.geofenceid;
+                  req.session.accesstypeid = user.accesstypeid;
+                  req.session.subgroupid = user.subgroupid;
+                });
+                console.log('accesstype',req.session.accesstype);
+                
+                console.log(req.session.isgeofence,'data');
+                return res.json({
+                  msg: "success",
+                  data: data,
+                });
+              } else {
+                return res.json({
+                  msg: "inactive",
+                });
+              }
+            } else {
+              return res.json({
+                msg: "resigned",
+              });
+            }
+          } else {
+            return res.json({
+              msg: "incorrect",
+            });
+          }
+        })
+        .catch((error) => {
+          return res.json({
+            msg: "error",
+            data: error,
+          });
+        });
+    });
+  } catch (error) {
+    res.json({
+      msg: error,
+    });
+  }
+});
+
+
+router.post("/logout", (req, res) => {
+  req.session.destroy((err) => {
+    if (err)
+      res.json({
+        msg: err,
+      });
+    res.json({
+      msg: "success",
+    });
+  });
+});
+
+
+
+
