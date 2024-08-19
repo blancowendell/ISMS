59df37967c5bdd8b6685ae3c9ba33a4fdcc73dd6
WendellBlanco <wendellblanco78@gmail.com>
2024-08-20 00:19:45 +0800

    [ADD COMPLETE VERIFICATION]

--- routes/finishapplication.js
+++ routes/finishapplication.js
@@ -21,7 +21,11 @@ module.exports = router;
 router.post("/loadexistingrecord", (req, res) => {
     try {
       let studentid = req.body.studentid;
-      let sql = `SELECT * FROM master_students
+      let sql = `SELECT 
+        *,
+        DATE_FORMAT(ms_date_of_birth, '%Y-%m-%d') AS ms_date_of_birth,
+        DATE_FORMAT(ms_registerdate, '%Y-%m-%d') AS ms_registerdate
+      FROM master_students
       WHERE ms_studentid = '${studentid}'`;
   
       Select(sql, (err, result) => {
@@ -48,72 +52,10 @@ router.post("/loadexistingrecord", (req, res) => {
   });
   
 
-router.post("/save", function (req, res, next) {
-  try {
-    const {
-      studentid,
-      image,
-      first_name,
-      middle_name,
-      last_name,
-      dob,
-      birthplace,
-      phone,
-      gender,
-      age,
-      school_name,
-      course_strand,
-      year_level,
-      academic_status,
-      register_date,
-      city,
-      barangay,
-      village,
-      street_address,
-      houseno,
-      fathersname,
-      fathersmonthly,
-      fathersoccupation,
-      mothersname,
-      mothersoccupation,
-      mothersmonthly,
-    } = req.body;
-    let scholar_status = 'Applied';
-
-    let sql = InsertStatement("master_students_request", "msr", [
-      "studentid",
-      "image",
-      "first_name",
-      "middle_name",
-      "last_name",
-      "date_of_birth",
-      "birthplace",
-      "phone",
-      "gender",
-      "age",
-      "status",
-      "institutionid",
-      "courseid",
-      "yearlevel",
-      "academic_status",
-      "registerdate",
-      "city",
-      "barangay",
-      "village",
-      "street",
-      "house_no",
-      "fathers_name",
-      "fathers_salary",
-      "fathers_occupation",
-      "mothers_name",
-      "mothers_occupation",
-      "mothers_salary",
-    ]);
-
-    let data = [
-      [
+  router.post("/save", function (req, res, next) {
+    try {
+      const {
         studentid,
-        image,
         first_name,
         middle_name,
         last_name,
@@ -122,7 +64,6 @@ router.post("/save", function (req, res, next) {
         phone,
         gender,
         age,
-        scholar_status,
         school_name,
         course_strand,
         year_level,
@@ -139,38 +80,122 @@ router.post("/save", function (req, res, next) {
         mothersname,
         mothersoccupation,
         mothersmonthly,
-      ],
-    ];
+        imageFile,
+        imageFileGradeCopy,
+        imageFileRegForm,
+        imageFileCertRes,
+        imageFileItr,
+        imageFileNfi,
+      } = req.body;
+      let scholar_status = 'Applied';
 
-    let checkStatement = SelectStatement(
-      "select * from master_students_request where msr_studentid=? and msr_status=?",
-      [studentid, scholar_status]
-    );
+      console.log('Request Body:', req.body);
 
-    Check(checkStatement)
-      .then((result) => {
-        console.log(result);
-        if (result != 0) {
-          return res.json(JsonWarningResponse(MessageStatus.EXIST));
-        } else {
-          InsertTable(sql, data, (err, result) => {
-            if (err) {
-              console.log(err);
-              res.json(JsonErrorResponse(err));
-            }
-
-            res.json(JsonSuccess());
-          });
-        }
-      })
-      .catch((error) => {
-        console.log(error);
-        res.json(JsonErrorResponse(error));
-      });
-  } catch (error) {
-    res.json(JsonErrorResponse(error));
-  }
-});
+      
+  
+      // Update the InsertStatement to include the new columns for files
+      let sql = InsertStatement("master_students_request", "msr", [
+        "studentid",
+        "image",
+        "first_name",
+        "middle_name",
+        "last_name",
+        "date_of_birth",
+        "birthplace",
+        "phone",
+        "gender",
+        "age",
+        "status",
+        "institutionid",
+        "courseid",
+        "yearlevel",
+        "academic_status",
+        "registerdate",
+        "city",
+        "baranggay",
+        "village",
+        "street",
+        "house_no",
+        "fathers_name",
+        "fathers_salary",
+        "fathers_occupation",
+        "mothers_name",
+        "mothers_occupation",
+        "mothers_salary",
+        "grade_copy",              // New field
+        "registration_form",       // New field
+        "certificate_residency",   // New field
+        "itr",                     // New field
+        "nfi"                      // New field
+      ]);
+  
+      // Update the data array to include the new Base64 fields
+      let data = [
+        [
+          studentid,
+          imageFile,
+          first_name,
+          middle_name,
+          last_name,
+          dob,
+          birthplace,
+          phone,
+          gender,
+          age,
+          scholar_status,
+          school_name,
+          course_strand,
+          year_level,
+          academic_status,
+          register_date,
+          city,
+          barangay,
+          village,
+          street_address,
+          houseno,
+          fathersname,
+          fathersmonthly,
+          fathersoccupation,
+          mothersname,
+          mothersoccupation,
+          mothersmonthly,
+          imageFileGradeCopy,
+          imageFileRegForm,
+          imageFileCertRes,
+          imageFileItr,
+          imageFileNfi,
+        ],
+      ];
+  
+      let checkStatement = SelectStatement(
+        "select * from master_students_request where msr_studentid=? and msr_status=?",
+        [studentid, scholar_status]
+      );
+  
+      Check(checkStatement)
+        .then((result) => {
+          if (result != 0) {
+            return res.json(JsonWarningResponse(MessageStatus.EXIST));
+          } else {
+            InsertTable(sql, data, (err, result) => {
+              if (err) {
+                console.log(err);
+                res.json(JsonErrorResponse(err));
+              }
+  
+              res.json(JsonSuccess());
+            });
+          }
+        })
+        .catch((error) => {
+          console.log(error);
+          res.json(JsonErrorResponse(error));
+        });
+    } catch (error) {
+      res.json(JsonErrorResponse(error));
+    }
+  });
+  
 
 //#region FUNCTION
 function Check(sql) {
